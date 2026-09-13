d91c1431d68abc64197ed038effcba545cebe17e
Melanie Elizabeth <melanietello2020@gmail.com>
2026-09-13 14:08:18 -0500

    agregar clase Producto

---
+++ app/src/main/java/com/example/storeperu/Producto.java
@@ -0,0 +1,80 @@
+package com.example.storeperu;
+
+public class Producto {
+
+    private int id;
+    private String nombre;
+    private String categoria;
+    private String descripcion;
+    private int garantia;
+    private double precio;
+    private int stock;
+
+    public Producto() {
+    }
+
+    public Producto(String nombre, String categoria, String descripcion, int garantia, double precio, int stock) {
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.descripcion = descripcion;
+        this.garantia = garantia;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getGarantia() {
+        return garantia;
+    }
+
+    public void setGarantia(int garantia) {
+        this.garantia = garantia;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+}
